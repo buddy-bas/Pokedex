74e15de3b18bdd832bbfc878dfb194439474c69b
Bundit <b.thakummee.96@gmail.com>
2023-01-14 15:14:28 +0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Bundit Thakummee on 14/1/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
